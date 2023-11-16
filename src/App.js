4cb29a9eaa671a1964e6612e4fcc89df12d19994
RomanKrivocheev <roman.krivocheev@gmail.com>
2023-11-16 19:01:11 +0100

    lastestsChanges

--- src/App.js
+++ src/App.js
@@ -2,14 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const containerStyle = {
+    position: 'relative',
+    width: '100%',
+    height: 0,
+    paddingTop: '56.2225%',
+    paddingBottom: 0,
+    boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+    margin: '1.6em 0.2em', // Adjusted margins for better spacing on mobile
+    overflow: 'hidden',
+    borderRadius: '8px',
+    willChange: 'transform',
+  };
+
+  const iframeStyle = {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: 0,
+    left: 0,
+    border: 'none',
+    padding: 0,
+    margin: 0,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Cirilla landing page!
-        </p>
-      </header>
+      <div style={containerStyle}>
+        <iframe
+          loading="lazy"
+          style={iframeStyle}
+          src="https://www.canva.com/design/DAF0PWxjs68/view?embed"
+          allowFullScreen
+          allow="fullscreen"
+        />
+      </div>
+      <a
+        href="https://www.canva.com/design/DAF0PWxjs68/view?utm_content=DAF0PWxjs68&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
     </div>
   );
 }
